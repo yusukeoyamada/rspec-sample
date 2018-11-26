6d442666a6aa95b9d215ef29cd538d437ec9ea8e
yusukeoyamada <fa19940118@gmail.com>
2018-11-26 21:07:56 +0900

    モデルテスト作成

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,34 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
+
   before do
     @user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
+     first_name: "Joe",
+     last_name: "Tester",
+     email: "joetester@example.com",
+     password: "dottle-nouveau-pavilion-tights-furze",
     )
 
     @project = @user.projects.create(
-      name: "Test Project",
+     name: "Test Project",
     )
   end
 
+  # バリデーションのテストが並ぶ
+  # ユーザー、プロジェクト、メッセージがあれば有効な状態であること
   it "is valid with a user, project, and message" do
     note = Note.new(
-      message: "This is a sample note.",
-      user: @user,
-      project: @project,
+     message: "This is a sample note.",
+     user: @user,
+     project: @project,
     )
     expect(note).to be_valid
   end
 
+  # メッセージがなければ無効な状態であること
   it "is invalid without a message" do
     note = Note.new(message: nil)
     note.valid?
     expect(note.errors[:message]).to include("can't be blank")
   end
 
+  # 文字列に一致するメッセージを検索する
   describe "search message for a term" do
     before do
       @note1 = @project.notes.create(
@@ -44,16 +49,17 @@ RSpec.describe Note, type: :model do
         user: @user,
       )
     end
-
+    # 一致するデータが見つかるとき
     context "when a match is found" do
+      # 検索文字列に一致するメモを返すこと
       it "returns notes that match the search term" do
-        expect(Note.search("first")).to include(@note1, @note3)
+       expect(Note.search("first")).to include(@note1, @note3)
       end
     end
-
+    # 一致するデータが1件も見つからないとき
     context "when no match is found" do
       it "returns an empty collection" do
-        expect(Note.search("message")).to be_empty
+       expect(Note.search("message")).to be_empty
       end
     end
   end
