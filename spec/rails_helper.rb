6ee7b7e9f7a61bd8f883bc4498d10d5b04b2dfec
yusukeoyamada <fa19940118@gmail.com>
2018-11-28 05:16:25 +0900

    APIテスト

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,6 +55,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  # コントローラスペックでDeviseのテストヘルパーを使用する
+  # Deviseのヘルパーメソッドをテスト内で使用する
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include RequestSpecHelper, type: :request
 end
