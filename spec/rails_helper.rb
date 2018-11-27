1e788d94894bc9159032663cfb6ceedb7d531acc
yusukeoyamada <fa19940118@gmail.com>
2018-11-27 21:15:26 +0900

    コントローラーテスト

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,4 +54,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # コントローラスペックでDeviseのテストヘルパーを使用する
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
