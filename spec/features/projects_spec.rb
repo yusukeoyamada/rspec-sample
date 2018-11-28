79f5c5d4cc9c184f1f0e4d9699b69cac5c9ca8b2
yusukeoyamada <fa19940118@gmail.com>
2018-11-29 01:25:31 +0900

    TDD開発練習

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -20,4 +20,23 @@ RSpec.feature "Projects", type: :feature do
       expect(page).to have_content "Owner: #{user.name}"
     end
   end
+  # ユーザーはプロジェクトを完了済みにする
+  scenario "user completes a project" do
+    # プロジェクトを持ったユーザーを準備する
+    user = FactoryBot.create(:user)
+    project = FactoryBot.create(:project, owner: user)
+    # ユーザーはログインしている
+    sign_in user
+    # ユーザーがプロジェクト画面を開き、
+    # "complete"ボタンをクリックすると、
+    visit project_path(project)
+    expect(page).to_not have_content "Completed"
+    click_button "Complete"
+    # プロジェクトは完了済みとしてマークされる
+    expect(project.reload.completed?).to be true
+    expect(page).to \
+      have_content "Congratulations, this project is complete!"
+    expect(page).to have_content "Completed"
+    expect(page).to_not have_button "Complete"
+  end
 end
